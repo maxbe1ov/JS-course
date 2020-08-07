044695d100bca8d8aa2f489db3c7787b440e027d
Max <shelbyrusky@gmail.com>
2020-08-07 17:55:35 +0300

    Первый проект на курсе

--- project1/script.js
+++ project1/script.js
@@ -1,46 +1,29 @@
-/*
-var budget = prompt("Какой у вас бюджет?",1000);
-var name = prompt("Название вашей фирмы?","У Ашота");
 
-let MainList = {
-		budget: budget,
-		name: name,
-		shopGoods: [],
-		employers: {}, 
-		open: true
-}
 
-for (let i = 0; i < 3; i++) {
-	var a = prompt(i + ") Какой тип товаров будем производить?");
+let money = +prompt("Ваш бюджет на месяц?","1000");
+let time = prompt("Введите дату в формате YYYY-MM-DD","2020.08.07");
+
+let appData = {
+	budget: money,
+	timeData: time,
+	expences: {},
+	optionalExpences: {},
+	income: [],
+	savings: false
+};
 	
-	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
-		console.log('Верно!');
-		MainList.shopGoods[i] = a;
-		console.log(MainList.shopGoods[i]);
-	} 
-	else {
-		console.log("Не верно! Так как ошибка " + a);
-		i--;
-	}
+	
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце","");
+	let b = prompt("Во сколько обойдется?","");
+
+	appData.expences[a] = b;
+	appData.budget -= b;
+	
 }
+console.log(appData.budget);
 
+appData.moneyPerDay = appData.budget / 30;
+alert("Ваш бюджет на 1 день :" + appData.moneyPerDay);
 
-
-budget = budget/30;
-alert("Бюджет вашей фирмы на 1 день " + budget + '$');*/
-
-/*var week = ["mon","tue","wen","thsd","frd","sun","strd"];
-for (let i = 0; i < 7; i++){
-	if (i === 5 || i === 6){
-		week[5] = "sunday";
-		week[6] = "saturday";
-	}
-	console.log(week[i]);
-}*/
-var arr = ["353","564","234","76","45","23","77"];
-for (let i = 0; i < 7; i++){
-	let a = arr[i];
-	if (a[0] === "3" || a[0] === "7") {
-		console.log(arr[i]);
-	}
-}
+console.log(appData);
